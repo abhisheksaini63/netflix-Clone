8ce12a663350b954c13193ec45f41dc8df16b9f0
abhisheksaini63 <93305926+abhisheksaini63@users.noreply.github.com>
2022-05-07 20:26:12 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+    <h1>Abhishek is a Student</h1>
+    <p>List of 5 Netflix series</p>
+    <ol>
+      <li>Doreamon</li>
+      <li>Chhota Bheem</li>
+      <li>Ninja Hatori</li>
+      <li>Tom And Jerry</li>
+      <li>Ben 10</li>
+    </ol>
+
+  </>,
+  document.getElementById('root')
+);
